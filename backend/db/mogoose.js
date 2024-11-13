fa22353b83843ecae181d954a776ed61d4af9beb
Naveen <152183513+naveen-166@users.noreply.github.com>
2024-11-13 10:07:21 +0530

    Update mogoose.js

--- backend/db/mogoose.js
+++ backend/db/mogoose.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 
 
-const mongourl='mongodb://localhost:27017/new';
+const mongourl='mongodb+srv://naveen_166:naveen123@resumeshortlist.nh03e.mongodb.net/?retryWrites=true&w=majority&appName=resumeshortlist';
 mongoose.connect(mongourl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=> console.log('mongodb connected'))
     .catch(err=>{console.log(err)});
